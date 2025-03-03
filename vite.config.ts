f01d3542f5e66ead0824957d441093461b13fe00
Jorge Guerra Canal <jorge.guerra.canal@gmail.com>
2025-03-04 00:06:16 +0100

    fix vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
@@ -10,12 +9,13 @@ export default defineConfig({
   ],
   server: {
     proxy: {
-      '/albums': {
+      '/api': {  // Cambiamos el endpoint base
         target: 'https://discana-api-346921755711.europe-west1.run.app',
         changeOrigin: true,
-        secure: false,
-        rewrite: (path) => path.replace(/^\/albums/, '/albums')
+        secure: true,  // Habilitamos SSL
+        rewrite: (path) => path.replace(/^\/api/, ''),
+        ws: true
       }
     }
   }
-});
+})
